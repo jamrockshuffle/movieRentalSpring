67cc94e5e2355b467822d33685f524c66c29ac72
KJ <sadsawewqe@asdsarew.com>
2024-05-24 13:54:31 +0300

    Transferred MovieRental project to spring

--- src/main/java/edu/pro/movierentalspring/model/Genre.java
+++ src/main/java/edu/pro/movierentalspring/model/Genre.java
@@ -1,7 +1,7 @@
 package edu.pro.movierentalspring.model;
 
 public enum Genre {
-    REGULAR, // Represents a regular movie genre
-    CHILDREN, // Represents a children's movie genre
-    NEW_RELEASE // Represents a new release movie genre
+    REGULAR,
+    CHILDREN,
+    NEW_RELEASE
 }
