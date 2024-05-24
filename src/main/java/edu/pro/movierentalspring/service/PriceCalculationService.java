ab8c33cb78a6b28ae970ca153245bef26b020929
KJ <sadsawewqe@asdsarew.com>
2024-05-24 13:58:17 +0300

    Transferred MovieRental project to spring

--- src/main/java/edu/pro/movierentalspring/service/PriceCalculationService.java
+++ src/main/java/edu/pro/movierentalspring/service/PriceCalculationService.java
@@ -7,12 +7,9 @@ import org.springframework.stereotype.Service;
 public class PriceCalculationService {
 
     public Double calculateTotalRentalPrice(Integer daysRented, RentalPricingPolicy pricingPolicy) {
-        // Check if the rental period is within the penalty-free duration
         if (daysRented <= pricingPolicy.getPenaltyThreshold()) {
-            // If within penalty-free duration, charge the standard price
             return pricingPolicy.getStandardRate();
         } else {
-            // If beyond penalty-free duration, calculate penalty price
             int daysWithPenalty = daysRented - pricingPolicy.getPenaltyThreshold();
             return pricingPolicy.getStandardRate() + (pricingPolicy.getPenaltyRate() * daysWithPenalty);
         }
